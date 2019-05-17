3ce8ae9582b93eb1884e3b9d7b47be3a9cd81136
Parker Helland <hellandp@gmail.com>
2019-05-17 11:44:49 -0700

    fetch works! quotes loading

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { PureComponent } from 'react';
+import TopQuotes from './containers/TopQuotes';
 
 export default class App extends PureComponent {
   render() {
     return (
       <>
+      <TopQuotes />
       </>
     );
   }
---
+++ src/components/containers/TopQuotes.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react';
+import Quotes from '../quotes/Quotes';
+import { getQuotes } from '../services/futuramaApi';
+
+export default class TopQuotes extends PureComponent {
+  state = {
+    quotes: []
+  }
+
+  componentDidMount() {
+    getQuotes()
+      .then(quotes => this.setState({ quotes }));
+  }
+
+  render() {
+    return <Quotes quotes={this.state.quotes} />;
+  } 
+}
